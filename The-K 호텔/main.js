97a5eef36faabd525122e70fe6f19306e0e44a5f
KimSungJin <86825816+aggie4@users.noreply.github.com>
2022-08-16 03:19:05 +0900

    Add files via upload

--- The-K 호텔/main.js
+++ The-K 호텔/main.js
@@ -1,1 +1,20 @@
 //setTimeout(showSlides, 2000);
+/* $('.list-Search').on('click', function () {
+    $('.active').css('display:block');
+}); */
+/* $('.inner .list-Search').click(function () {
+    let d = $(this).siblings('active').css('display');
+    if (d == 'block') {
+        return false;
+    }
+}); */
+/* let a = $('.inner .list-Search');
+console.log('버튼 클릭이 되나여?', a);
+let b = $('.active');
+console.log('이건 되나여?', b); */
+$('.inner .list-Search').click(function () {
+    $('.active').css({
+        display: 'block',
+        height: 260,
+    });
+});
